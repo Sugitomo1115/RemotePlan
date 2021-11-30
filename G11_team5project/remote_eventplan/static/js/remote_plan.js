80d0055f48c632a08ec2d02e5905ed2858bc6650
Tomoya Sugita <s1f101901937@iniad.org>
2021-12-01 00:00:14 +0900

    Create remote_plan.js
    like機能の追加

---
+++ G11_team5project/remote_eventplan/static/js/remote_plan.js
@@ -0,0 +1,15 @@
+function callback(json){
+	var element=document.getElementById('like'+json.id);
+	element.textContent=json.like;}
+
+function like(plan_id){
+	var req=new XMLHttpRequest();
+	req.onreadystatechange=function(){
+		if((req.readyState==4)&&(req.status==200)){
+			var json=JSON.parse(req.responseText);
+			callback(json);
+		}
+	}
+	req.open('GET','/api/plans/'+plan_id+'/like');
+	req.send(null);
+}

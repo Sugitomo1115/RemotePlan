22c77f8c56822a42e4e5007c8123770d3594668d
Tomoya Sugita <s1f101901937@iniad.org>
2021-12-01 00:29:37 +0900

    Update urls.py
    トップ画面、作成画面、詳細画面、イイね機能、削除機能へのurlsを追加

--- G11_team5project/remote_eventplan/urls.py
+++ G11_team5project/remote_eventplan/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path(r'', views.top, name="top")
+    path('', views.top, name="top"),
+    path('create/', views.create, name="create"),
+    path('<int:plan_id>/', views.detail, name="detail"),
+    path('<int:plan_id>/like', views.like,name='like'),
+    path('api/plans/<int:plan_id>/like', views.api_like),
+    path('<int:plan_id>/delete', views.delete, name='delete'),
 ]

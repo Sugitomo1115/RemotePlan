83df97d38ed1020b5d62e8b7e9268d945fd4a786
Tomoya Sugita <s1f101901937@iniad.org>
2021-11-30 14:19:19 +0900

    Update models.py
    Planモデルを追加

--- G11_team5project/remote_eventplan/models.py
+++ G11_team5project/remote_eventplan/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Plan(models.Model):
+    name = models.CharField(max_length=100)
+    target = models.CharField(max_length=20)
+    person = models.CharField(max_length=20)
+    time = models.CharField(max_length=100)
+    tools = models.CharField(max_length=100)
+    help = models.CharField(max_length=100)
+    outline = models.CharField(max_length=200)
+    posted_at = models.DateTimeField('date published')
+    like = models.IntegerField(default=0)

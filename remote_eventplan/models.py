7f77ecf09bfe7bc80ab412b1e1c3d7e1f33c24aa
Tomoya Sugita <s1f101901937@iniad.org>
2021-12-02 20:13:21 +0900

    Update sqlite3

--- remote_eventplan/models.py
+++ remote_eventplan/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Plan(models.Model):
-    name = models.CharField(max_length=100)
-    target = models.CharField(max_length=20)
-    person = models.CharField(max_length=20)
-    category = models.CharField(max_length=30)
-    time = models.CharField(max_length=100)
-    tools = models.CharField(max_length=100)
-    help = models.CharField(max_length=100)
-    outline = models.CharField(max_length=200)
+    name = models.CharField(max_length=100, default="")
+    target = models.CharField(max_length=20, default="誰でも")
+    person = models.CharField(max_length=20, default="")
+    category = models.CharField(max_length=30, default="その他")
+    time = models.CharField(max_length=100, default="")
+    tools = models.CharField(max_length=100, default="")
+    help = models.CharField(max_length=100, default="")
+    outline = models.CharField(max_length=200, default="")
     posted_at = models.DateTimeField('date published')
     like = models.IntegerField(default=0)

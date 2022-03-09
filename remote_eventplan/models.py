b424b37776729e64075092f6823b7dab63de63f2
Tomoya SUGITA <s1f101901937@iniad.org>
2022-03-09 15:45:17 +0900

    Fixed model.py

--- remote_eventplan/models.py
+++ remote_eventplan/models.py
@@ -1,5 +1,3 @@
-from pyexpat import model
-from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
 from django.contrib.auth.validators import UnicodeUsernameValidator
